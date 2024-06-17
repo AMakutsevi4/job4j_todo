290bc5db64c28dd2044c33d2b526029f4f51ed4d
Alexandr <amakutsevich@bk.ru>
2024-06-17 08:54:36 +0700

    Add Lombok

--- src/main/java/ru/job4j/todo/model/Item.java
+++ src/main/java/ru/job4j/todo/model/Item.java
@@ -1,9 +1,11 @@
 package ru.job4j.todo.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.Date;
-import java.util.Objects;
 
+@Data
 @Entity
 @Table(name = "todo")
 public class Item {
@@ -16,72 +18,4 @@ public class Item {
     private Date created;
     private boolean done;
 
-    public Item() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Date getCreated() {
-        return created;
-    }
-
-    public void setCreated(Date created) {
-        this.created = created;
-    }
-
-    public boolean isDone() {
-        return done;
-    }
-
-    public void setDone(boolean done) {
-        this.done = done;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Item item = (Item) o;
-        return id == item.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Item{"
-                + "id=" + id
-                + ", description='" + description
-                + '\''
-                + '}';
-    }
 }
